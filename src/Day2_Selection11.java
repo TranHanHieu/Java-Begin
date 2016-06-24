6790820676c891a8c71b5aafcfff05de6dbe87d3
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-06-24 16:09:28 +0700

    first commit

--- src/Day2_Selection11.java
+++ src/Day2_Selection11.java
@@ -6,14 +6,10 @@ import java.util.Scanner;
 public class Day2_Selection11 {
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
-        double F;
-        double C;
         System.out.println("Chương trình đổi độ F sang độ C.");
         System.out.println("Nhập độ (F) cần đổi : ");
-        F = Input.nextDouble();
-        C = (F - 32)/1.8;
+        double F = Input.nextDouble();
+        double C = (F - 32)/1.8;
         System.out.println("Đổi " + F + " (F) = " + C + " (C) ");
-
-
     }
 }
