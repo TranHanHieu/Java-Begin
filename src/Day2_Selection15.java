c099c294ee3b7181d19a4469c0475c70b3e8e05f
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-06-24 16:20:06 +0700

    first commit

--- src/Day2_Selection15.java
+++ src/Day2_Selection15.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Created by Hieu It on 6/24/2016.
  */
 public class Day2_Selection15 {
-    public static void Tinh_BMI(){
+    public static void main(String[] args) {
         Scanner scanIn = new Scanner(System.in);
         System.out.println("Chương trình tính sự cân bằng giữa chiều cao và cân nặng của một người");
         System.out.println("Nhập Họ và tên : " );
@@ -14,16 +14,20 @@ public class Day2_Selection15 {
         System.out.println("Nhập cân nặng (kg) : ");
         double weight = scanIn.nextDouble();
         double BMI = (weight)/(hight*hight);
-        if (BMI < 18.5)
-            System.out.println( name + ". Bạn là người thiếu cân, bạn đang quá gầy !");
-        if (BMI >= 18.5 && BMI < 25)
-            System.out.println(name + ". Bạn có thân hình thật lí tưởng, thật tuyệt !");
-        if (BMI >= 25 && BMI <=30)
-            System.out.println(name + ". Bạn đang thừa cân rồi !");
-        if (BMI > 30)
-            System.out.println(name + ". Bạn đang bị béo phì, cần giảm cân !");
-    }
-    public static void main(String[] args) {
-        Tinh_BMI();
+        if (BMI < 18.5) {
+            System.out.println(name + ". Bạn là người thiếu cân, bạn đang quá gầy !");
+        } else {
+            if (BMI >= 18.5 && BMI < 25) {
+                System.out.println(name + ". Bạn có thân hình thật lí tưởng, thật tuyệt !");
+            } else {
+                if (BMI >= 25 && BMI <= 30) {
+                    System.out.println(name + ". Bạn đang thừa cân rồi !");
+                } else {
+                    if (BMI > 30) {
+                        System.out.println(name + ". Bạn đang bị béo phì, cần giảm cân !");
+                    }
+                }
+            }
+        }
     }
 }
