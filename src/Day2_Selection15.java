6c392778a5113273dea77189e20d6a15b3b5df1c
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-06-26 02:20:10 +0700

    first commit

--- src/Day2_Selection15.java
+++ src/Day2_Selection15.java
@@ -7,20 +7,20 @@ public class Day2_Selection15 {
     public static void main(String[] args) {
         Scanner scanIn = new Scanner(System.in);
         System.out.println("Chương trình tính sự cân bằng giữa chiều cao và cân nặng của một người");
-        System.out.println("Nhập Họ và tên : " );
+        System.out.println("Nhập Họ và tên : ");
         String Name = scanIn.nextLine();
         System.out.println("Nhập chiều cao (m): ");
         double Hight = scanIn.nextDouble();
         System.out.println("Nhập cân nặng (kg) : ");
         double Weight = scanIn.nextDouble();
-        double BMI = (Weight)/(Hight*Hight);
+        double BMI = (Weight) / (Hight * Hight);
         if (BMI < 18.5) {
             System.out.println(Name + ". Bạn là người thiếu cân, bạn đang quá gầy !");
         } else {
-            if (BMI >= 18.5 && BMI < 25) {
+            if (BMI < 25) {
                 System.out.println(Name + ". Bạn có thân hình thật lí tưởng, thật tuyệt !");
             } else {
-                if (BMI >= 25 && BMI <= 30) {
+                if (BMI <= 30) {
                     System.out.println(Name + ". Bạn đang thừa cân rồi !");
                 } else {
                     if (BMI > 30) {
--- src/Day2_Selection18.java
+++ src/Day2_Selection18.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Scanner;
 
 /**
@@ -5,18 +7,10 @@ import java.util.Scanner;
  */
 public class Day2_Selection18 {
     public static void main(String[] args) {
-        Scanner scanIn= new Scanner(System.in);
+        Scanner scanIn = new Scanner(System.in);
         System.out.println("Nhập năm :");
-        boolean a = false;
         int nam = scanIn.nextInt();
-        while (nam % 4 == 0 && nam % 100 != 0 || nam % 400 == 0) {
-            System.out.println("True !");
-            a = true;
-            break;
-        }
-        while (!a) {
-            System.out.println("False !");
-            break;
-        }
+        boolean a = (nam % 4 == 0 && nam % 100 != 0 || nam % 400 == 0);
+        System.out.println(a);
     }
 }
