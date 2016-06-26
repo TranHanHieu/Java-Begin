b2287549c59ae7a49cb7408db35ca5a9912861e2
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-06-26 11:37:32 +0700

    first commit

--- src/Day3_Selection4.java
+++ src/Day3_Selection4.java
@@ -8,15 +8,13 @@ public class Day3_Selection4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Nhập vào chuỗi 1 : ");
-        String a = input.next();
+        String a = input.nextLine();
         System.out.println("Nhập vào chuỗi 2 : ");
-        String b = input.next();
+        String b = input.nextLine();
         if (b.compareTo(a) <= 0) {
             System.out.println("Chuỗi 2 là substring của chuỗi 1 ");
         } else {
             System.out.println("Chuỗi 2 không là substring của chuỗi 1 ");
         }
-        System.out.println(a);
-        System.out.println(b);
     }
 }
