f0c74e4e5979a3321d8154b8936354b29bea6952
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-06-30 21:47:12 +0700

    first commit

--- src/Day4_Selection20.java
+++ src/Day4_Selection20.java
@@ -4,30 +4,31 @@ import java.util.Scanner;
  * Created by Hieu It on 6/27/2016.
  */
 public class Day4_Selection20 {
-    public static void reverseArray(String[] A) {
-        String[] B = new String[A.length];
-        for (int i = 0; i < A.length; i++) {
-            B[i] = A[A.length - 1 - i];
+    public static String[] reverseArray(String[] arr) {
+        String[] array = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            array[i] = arr[arr.length - 1 - i];
         }
         System.out.println();
         System.out.println("Chuỗi đảo ngược của chuỗi A là : ");
-        for (int i = 0; i < B.length; i++) {
-            System.out.print(B[i] + " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(array[i] + " ");
         }
+        return arr;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Nhập số phần tử chuỗi A : ");
-        int c = input.nextInt();
-        String[] A = new String[c];
-        for (int i = 0; i < c; i++) {
+        int length = input.nextInt();
+        String[] arr = new String[length];
+        for (int i = 0; i < length; i++) {
             System.out.println("Nhập phần tử thứ " + (i + 1) + " :");
-            A[i] = input.next();
+            arr[i] = input.next();
         }
         System.out.println("Chuỗi A là : ");
-        for (int i = 0; i < c; i++) {
-            System.out.print(A[i] + " ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        reverseArray(A);
+        reverseArray(arr);
     }
 }
