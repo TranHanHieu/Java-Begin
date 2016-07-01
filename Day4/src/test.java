124939be028fc93ece3552c8eedffc0a774fbba9
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-07-02 00:47:00 +0700

    first commit

---
+++ Day4/src/test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Hieu It on 7/1/2016.
+ */
+public class test {
+    public static void main(String[] args) {
+        int b  = 1;
+        for ( ;;){
+            System.out.println(b);
+        }
+    }
+}
