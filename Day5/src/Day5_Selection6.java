825c1119ab098ef14878fe52d0667ce091b8b83b
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-07-04 15:18:09 +0700

    first commit

--- Day5/src/Day5_Selection6.java
+++ Day5/src/Day5_Selection6.java
@@ -24,11 +24,24 @@ public class Day5_Selection6 {
         String s2 = "";
         //System.out.println(index);
         for (int i = 0; i < index; i++) {
+            if (add.charAt(i)==' '){
+                i++;
+                if(i==index) {
+                    break;
+                }
+            }
             s1 += add.charAt(i);
         }
         for (int i = index + 1; i < arr.length; i++) {
+            if (add.charAt(i)==' '){
+                i++;
+                if(i==arr.length) {
+                    break;
+                }
+            }
             s2 += add.charAt(i);
         }
+        //System.out.println(s1);
         if (arr[index] == '+') {
             System.out.println("Kết quả");
             result = Double.valueOf(s1) + Double.valueOf(s2);
