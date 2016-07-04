253efe2d825c8702156f6728eff829f468876516
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-07-04 14:37:27 +0700

    first commit

--- Day5/src/Day5_Selection6.java
+++ Day5/src/Day5_Selection6.java
@@ -5,7 +5,52 @@ import java.util.Scanner;
  */
 public class Day5_Selection6 {
     public static void main(String[] args) {
+        char[] arr;
+        int index = 0;
+        double result = 0;
+        //int a = 0, b = 0;
         Scanner input = new Scanner(System.in);
-        String add = input.next();
+        String add = input.nextLine();
+        arr = add.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '+' || arr[i] == '-' || arr[i] == 'x' || arr[i] == ':') {
+                index = i;
+            }
+        }
+        if (arr[index] != '+' && arr[index] != '-' && arr[index] != 'x' && arr[index] != ':') {
+            System.out.println("Bạn nhập sai toán tử !");
+        }
+        String s1 = "";
+        String s2 = "";
+        //System.out.println(index);
+        for (int i = 0; i < index; i++) {
+            s1 += add.charAt(i);
+        }
+        for (int i = index + 1; i < arr.length; i++) {
+            s2 += add.charAt(i);
+        }
+        if (arr[index] == '+') {
+            System.out.println("Kết quả");
+            result = Double.valueOf(s1) + Double.valueOf(s2);
+            System.out.println(result);
+        } else {
+            if (arr[index] == '-') {
+                System.out.println("Kết quả :");
+                result = Double.valueOf(s1) - Double.valueOf(s2);
+                System.out.println(result);
+            } else {
+                if (arr[index] == 'x') {
+                    System.out.println("Kết quả :");
+                    result = Double.valueOf(s1) * Double.valueOf(s2);
+                    System.out.println(result);
+                } else {
+                    if (arr[index] == ':') {
+                        System.out.println("Kết quả :");
+                        result = Double.valueOf(s1) / Double.valueOf(s2);
+                        System.out.println(result);
+                    }
+                }
+            }
+        }
     }
 }
