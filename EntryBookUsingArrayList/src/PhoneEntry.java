c7b21e8d6da1e5f81bc3cff11fb7ddea66d9bec7
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-07-15 12:17:50 +0700

    first commit

---
+++ EntryBookUsingArrayList/src/PhoneEntry.java
@@ -0,0 +1,35 @@
+/**
+ * Created by ThangNguyen on 7/11/2016.
+ */
+public class PhoneEntry {
+    private String name;
+    private String phoneNumber;
+
+    PhoneEntry() {
+    }
+
+    PhoneEntry(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String toString() {
+        return "\tTên : "+this.getName() + "\t---> " + "Số điện thoại : "+ this.getPhoneNumber();
+    }
+}
